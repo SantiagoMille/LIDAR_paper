bbbfcd6e2dbf0c20e024ce695a8c41ea9e6dd35c
Luis Santiago Mille Fragoso <lsmille@Luiss-MacBook-Pro.local>
2024-11-28 01:19:06 -0800

    updated transcriptome analysis code

--- Transcriptome_specificity_profile/preprocess_redi.py
+++ Transcriptome_specificity_profile/preprocess_redi.py
@@ -3,13 +3,8 @@ import pandas as pd
 
 files = sys.argv[1:]
 
-def get_nonzero(x):
-    #nonzero = x[x["Frequency"] > 0.99]
-    nonzero = x[x["BaseCount[A,C,G,T]"]]
-    return nonzero
-
 def get_adar_edits(x):
-    #adarE = x[ (x["AllSubs"] == 'AG') | (x["AllSubs"] == 'TC') ]
+    # if AllSubs isn't empty (from REDI) there was a detected base change
     adarE = x[ x["AllSubs"].str.contains('AG') | x["AllSubs"].str.contains('TC') ]
     #adarE = x[ (x["Reference"] == 'A') | (x["Reference"] == 'T') ]
     return adarE
@@ -17,7 +12,7 @@ def get_adar_edits(x):
 def main():
     for file in files:
 
-        print(file)
+        print(file) #read file that is output from REDI
         df = pd.read_csv(file, sep="\t", chunksize=5000)
         edits = pd.DataFrame()
         c = 1
@@ -30,21 +25,7 @@ def main():
         del adar_edits
 
         print(c)
-        #print(len(df_2s))
         print(len(edits))
         edits.to_csv(file+".filtered_edits.csv")
 
-def quick_an():
-    for file in files:
-
-        print(file)
-        df = pd.read_csv(file, sep="\t", chunksize=5000)
-        edits = pd.DataFrame()
-        c = 1
-        for chunk in df:
-            adar_edits = get_nonzero(chunk)
-            edits = pd.concat([edits, adar_edits])
-    print(len(edits))
-
 main()
-#quick_an()
